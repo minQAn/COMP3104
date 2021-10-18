f30e7d27271ebf3a5c193bd133ddee28dee8172d
minQ <minq.an7@gmail.com>
2021-10-18 03:28:26 -0400

    updated build folder

---
+++ build/precache-manifest.a433521719af971b188e7c804f02d662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e201d36935b02a40605a",
+    "url": "/minQAn/COMP3104/static/css/main.78e43eff.chunk.css"
+  },
+  {
+    "revision": "e201d36935b02a40605a",
+    "url": "/minQAn/COMP3104/static/js/main.e201d369.chunk.js"
+  },
+  {
+    "revision": "355df900a93541264837",
+    "url": "/minQAn/COMP3104/static/js/runtime~main.355df900.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/minQAn/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4fc1f08c7a52b7f392630fa89b2c9133",
+    "url": "/minQAn/COMP3104/index.html"
+  }
+];
