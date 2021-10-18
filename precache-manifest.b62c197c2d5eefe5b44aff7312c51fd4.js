42c4d6878b2e1e82968dacabb0cae6cf1b988312
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-18 07:30:23 +0000

    Deploy minQAn/COMP3104 to github.com/minQAn/COMP3104.git:gh-pages

---
+++ precache-manifest.b62c197c2d5eefe5b44aff7312c51fd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c6af33c685108b45dd7",
+    "url": "/minQAn/COMP3104/static/css/main.88fc3e9d.chunk.css"
+  },
+  {
+    "revision": "1c6af33c685108b45dd7",
+    "url": "/minQAn/COMP3104/static/js/main.1c6af33c.chunk.js"
+  },
+  {
+    "revision": "355df900a93541264837",
+    "url": "/minQAn/COMP3104/static/js/runtime~main.355df900.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/minQAn/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "6c280861cfbb46094c2aaf93afb1b6bb",
+    "url": "/minQAn/COMP3104/index.html"
+  }
+];
